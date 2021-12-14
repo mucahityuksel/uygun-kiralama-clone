89810ae8542ed93c883d6003b9516561f0687141
mucahityuksel <m.yuksel793@gmail.com>
2021-12-14 11:25:51 +0300

    checked

--- src/components/InformationComponent/index.js
+++ src/components/InformationComponent/index.js
@@ -1,6 +1,6 @@
-import React,{useState} from 'react'
-import {VscCircleFilled} from "react-icons/all"
-import Carousel,{Dots} from '@brainhubeu/react-carousel';
+import React, { useState } from 'react'
+import { VscCircleFilled } from "react-icons/all"
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 import image1 from "../../images/body-image.png";
 import image2 from "../../images/body-images.png"
@@ -8,22 +8,22 @@ import "./style.css"
 
 function InformationComponent() {
 
-  const [value,setValue] = useState(0);
+  const [value, setValue] = useState(0);
   const slides = [
     (<div className="inline-container1">
-          <div className="image-text first">Furniture</div>
-          <div className="image-text second">Work <br></br>Smarter Save Smarter</div>
-          <div className="image-text third">Get %10 off every month</div>
-          <div className="image-text"><button className="info-button">Know More</button></div>
+      <div className="image-text first">Furniture</div>
+      <div className="image-text second">Work <br></br>Smarter Save Smarter</div>
+      <div className="image-text third">Get %10 off every month</div>
+      <div className="image-text"><button className="info-button">Know More</button></div>
     </div>),
     (<div className="inline-container2">
-          <div className="image-text first">Furniture</div>
-          <div className="image-text second">Work <br></br>Smarter Save Smarter</div>
-          <div className="image-text third">Get %10 off every month</div>
-          <div className="image-text"><button className="info-button">Know More</button></div>
+      <div className="image-text first">Furniture</div>
+      <div className="image-text second">Work <br></br>Smarter Save Smarter</div>
+      <div className="image-text third">Get %10 off every month</div>
+      <div className="image-text"><button className="info-button">Know More</button></div>
     </div>),
   ]
-  const onchange = (value)=> {
+  const onchange = (value) => {
     setValue(value)
   }
   return (
@@ -33,7 +33,7 @@ function InformationComponent() {
         slides={slides}
         onChange={onchange}
       />
-      
+
       <Dots value={value} onChange={onchange} number={slides.length} />
     </div>
   )
